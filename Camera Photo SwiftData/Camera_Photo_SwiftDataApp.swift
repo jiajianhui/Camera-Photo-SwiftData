0ba91d0b0ab938e74f9e883f05180b583078e4b4
jianhui <78072865+jiajianhui@users.noreply.github.com>
2025-07-10 10:45:57 +0800

    Initial Commit

---
+++ Camera Photo SwiftData/Camera_Photo_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_Photo_SwiftDataApp.swift
+//  Camera Photo SwiftData
+//
+//  Created by 贾建辉 on 2025/7/10.
+//
+
+import SwiftUI
+
+@main
+struct Camera_Photo_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
